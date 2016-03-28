1115834ceb7c32cbeb8ecb832a3c1d623acf6425
User <user@users-mac-pro.attlocal.net>
2016-03-28 15:44:47 -0700

    Signed-off-by: User <user@users-mac-pro.attlocal.net>

---
+++ iss_tracker_27_copy_(2)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iss_tracker_27_copy_(2)
+//
+//  Created by User on 3/21/16.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iss_tracker_27_copy_(2)/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  iss_tracker_27_copy_(3)
+//
+//
+//  Cloud L. Cho in Burbank, California
+//  coding from 3/24/2016 to 3/24/2016
+//  (3) not work
+/*
+ not updating the current location - 3/24/2016
+ duplicated satellite location - 3/24/2016
+ 
+ to add the current user location
+
+ 
+ */
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool
+    {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        //return UIApplicationMain(argc, argv, nil, NSStringFromClass([Lesson27AppDelegate class]));
+        
+    }
+}
